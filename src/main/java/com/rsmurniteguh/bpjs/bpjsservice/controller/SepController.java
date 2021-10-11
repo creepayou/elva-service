d2cec31d346723ea1f4ed5e11a05ab62ddfbffc0
Eric <eric.it@rsmurniteguh.com>
2021-10-11 16:06:56 +0700

    SepController: Ubah agar menggunakan pathvariable

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/controller/SepController.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/controller/SepController.java
@@ -11,12 +11,12 @@ import com.rsmurniteguh.bpjs.bpjsservice.util.VClaimResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.apachecommons.CommonsLog;
@@ -32,8 +32,8 @@ public class SepController {
     @Autowired
     private BpjsConsumerService bpjsConsumerService;
 
-    @GetMapping("/searchSEP")
-    public ResponseSts<BpjsSepDto> searchSEP(@RequestParam("sepNo") String sepNo,
+    @GetMapping("/searchSEP/{sepNo}")
+    public ResponseSts<BpjsSepDto> searchSEP(@PathVariable("sepNo") String sepNo,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) {
         try {
             return ResponseSts
