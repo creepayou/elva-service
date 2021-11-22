fb8e47fb9f17fd098c6cf05735096630695e27ad
Eric <eric.it@rsmurniteguh.com>
2021-11-22 10:07:37 +0700

    Penambahan interceptor logging request

---
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/interceptor/RequestLoggingInterceptor.java
@@ -0,0 +1,59 @@
+package com.rsmurniteguh.bpjs.bpjsservice.interceptor;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.rsmurniteguh.bpjs.bpjsservice.base.constant.Constant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import lombok.extern.apachecommons.CommonsLog;
+
+@Component
+@CommonsLog
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        request.setAttribute("startTime", System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable Exception ex) {
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long executeTime = endTime - startTime;
+        String logString = String.format(
+                "Method: %s, URI: %s, Entity: %s, Request Time: %d ms, Status: %d, Parameter: %s, Host: %s %s",
+                request.getMethod(), request.getRequestURI(), request.getHeader(Constant.MT_ENTITY_CODE), executeTime,
+                response.getStatus(), parameterString(request.getParameterMap()), request.getRemoteHost(),
+                request.getRemoteAddr());
+        if (!HttpStatus.valueOf(response.getStatus()).isError()) {
+            log.info(logString);
+        } else {
+            log.error(logString);
+        }
+    }
+
+    private String parameterString(Map<String, String[]> parameterMap) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (!parameterMap.isEmpty()) {
+            for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+                stringBuilder.append(entry.getKey() + ": " + entry.getValue()[0] + ",");
+            }
+        }
+
+        String parameterString = stringBuilder.toString();
+        if (parameterString.lastIndexOf(',') != -1) {
+            return "(" + parameterString.substring(0, parameterString.lastIndexOf(',')) + ")";
+        }
+        return stringBuilder.toString();
+    }
+}
