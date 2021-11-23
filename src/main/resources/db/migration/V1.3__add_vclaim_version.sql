945ddaf4900ca882feddb0e94ba15bacc282687a
Eric <eric.it@rsmurniteguh.com>
2021-11-23 11:44:44 +0700

    Migration: Fix sql untuk set default value

--- src/main/resources/db/migration/V1.3__add_vclaim_version.sql
+++ src/main/resources/db/migration/V1.3__add_vclaim_version.sql
@@ -1,5 +1,5 @@
 ALTER TABLE bpjsconsumer
-ADD COLUMN vclaim_version VARCHAR(50) NOT NULL SET DEFAULT("1.1");
+ADD COLUMN IF NOT EXISTS vclaim_version VARCHAR(50) NOT NULL DEFAULT('1.1');
 
 ALTER TABLE bpjsconsumer
 ALTER COLUMN provider_code SET NOT NULL;
