9259c685374543fada69d8e36160a759f953c5ce
Eric <eric.it@rsmurniteguh.com>
2021-11-24 11:11:33 +0700

    Penambahan case jika gagal auth agar error tidak aneh dan menyesuaikan nama variable

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/util/VClaimResponseUtil.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/util/VClaimResponseUtil.java
@@ -2,6 +2,7 @@ package com.rsmurniteguh.bpjs.bpjsservice.util;
 
 import java.util.Map;
 
+import com.rsmurniteguh.bpjs.bpjsservice.base.constant.Constant;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.response.VClaimResponse;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.response.VClaimResponse2;
 import com.rsmurniteguh.bpjs.bpjsservice.exception.BpjsServiceException;
@@ -11,7 +12,7 @@ public class VClaimResponseUtil {
     private VClaimResponseUtil(){}
 
     public static <T> Map<String, T> handleVClaimResponse(VClaimResponse<T> vClaimResponse) throws BpjsServiceException {
-        if(vClaimResponse.getMetaData().getCode().equals("200")){
+        if(vClaimResponse.getMetaData().getCode().equals(Constant.HTTP_OK)){
             return vClaimResponse.getResponse();
         } else {
             throw new BpjsServiceException(vClaimResponse.getMetaData().getMessage());
@@ -19,7 +20,7 @@ public class VClaimResponseUtil {
     }
 
     public static <T> T handleVClaimResponse(VClaimResponse2<T> vClaimResponse) throws BpjsServiceException {
-        if(vClaimResponse.getMetaData().getCode().equals("200")){
+        if(vClaimResponse.getMetaData().getCode().equals(Constant.HTTP_OK)){
             return vClaimResponse.getResponse();
         } else {
             throw new BpjsServiceException(vClaimResponse.getMetaData().getMessage());
