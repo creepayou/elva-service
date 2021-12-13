430acde937ef1e84d3856797b932d6d43a159a30
Eric <eric.it@rsmurniteguh.com>
2021-12-13 11:38:58 +0700

    EnumController: Penambahan enum controller untuk load enum yang ada pada bpjsservice

---
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/controller/EnumController.java
@@ -0,0 +1,69 @@
+package com.rsmurniteguh.bpjs.bpjsservice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rsmurniteguh.bpjs.bpjsservice.base.constant.Constant;
+import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.JenisPelayanan;
+import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.Lakalantas;
+import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.StatusKlaim;
+import com.rsmurniteguh.bpjs.bpjsservice.dto.model.VClaimMappingDto;
+import com.rsmurniteguh.bpjs.bpjsservice.dto.response.ResponseSts;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.apachecommons.CommonsLog;
+
+@RestController
+@CommonsLog
+@RequestMapping("/enum")
+public class EnumController {
+
+    @GetMapping("/getJenisPelayanan")
+    public ResponseSts<List<VClaimMappingDto>> getJenisPelayanan(@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) {
+        try {
+            List<VClaimMappingDto> vclaimMappingDtoList = new ArrayList<>();
+            for(JenisPelayanan item : JenisPelayanan.values()) {
+                VClaimMappingDto dto = new VClaimMappingDto(item.name(), item.getJenis().getNama());
+                vclaimMappingDtoList.add(dto);
+            }
+            return ResponseSts.onSuccess(vclaimMappingDtoList);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return ResponseSts.onFail(e.getMessage());
+        }
+    }
+    
+    @GetMapping("/getStatusKlaim")
+    public ResponseSts<List<VClaimMappingDto>> getStatusKlaim(@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) {
+        try {
+            List<VClaimMappingDto> vclaimMappingDtoList = new ArrayList<>();
+            for(StatusKlaim item : StatusKlaim.values()) {
+                VClaimMappingDto dto = new VClaimMappingDto(item.name(), item.getStatus().getNama());
+                vclaimMappingDtoList.add(dto);
+            }
+            return ResponseSts.onSuccess(vclaimMappingDtoList);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return ResponseSts.onFail(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getLakaLantas")
+    public ResponseSts<List<VClaimMappingDto>> getLakaLantas(@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) {
+        try {
+            List<VClaimMappingDto> vclaimMappingDtoList = new ArrayList<>();
+            for(Lakalantas item : Lakalantas.values()) {
+                VClaimMappingDto dto = new VClaimMappingDto(item.name(), item.getKll().getNama());
+                vclaimMappingDtoList.add(dto);
+            }
+            return ResponseSts.onSuccess(vclaimMappingDtoList);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return ResponseSts.onFail(e.getMessage());
+        }
+    }
+}
