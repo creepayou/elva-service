1e15d68737c41775df92260170f4b74b11d5fed0
Eric <eric.it@rsmurniteguh.com>
2021-12-23 11:46:01 +0700

    Fix model BpjsKelasRawatDto

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/dto/model/BpjsKelasRawatDto.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/dto/model/BpjsKelasRawatDto.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.rsmurniteguh.bpjs.bpjsservice.config.CustomBpjsEnumSerializer;
-import com.rsmurniteguh.bpjs.bpjsservice.config.EmptyIfNull;
+import com.rsmurniteguh.bpjs.bpjsservice.config.CustomNullSerializer;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.KelasRawat;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.Pembiayaan;
 
@@ -17,10 +17,12 @@ import lombok.experimental.Accessors;
 @JsonInclude(value = Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BpjsKelasRawatDto {
-    @JsonSerialize(using = CustomBpjsEnumSerializer.class)
+    @JsonSerialize(using = CustomBpjsEnumSerializer.class, nullsUsing = CustomNullSerializer.class)
     private KelasRawat klsRawatHak;
-    private @EmptyIfNull String klsRawatNaik;
-    @JsonSerialize(using = CustomBpjsEnumSerializer.class)
-    private @EmptyIfNull Pembiayaan pembiayaan;
-    private @EmptyIfNull String penanggungJawab;
+    @JsonSerialize(nullsUsing = CustomNullSerializer.class)
+    private String klsRawatNaik;
+    @JsonSerialize(using = CustomBpjsEnumSerializer.class, nullsUsing = CustomNullSerializer.class)
+    private Pembiayaan pembiayaan;
+    @JsonSerialize(nullsUsing = CustomNullSerializer.class)
+    private String penanggungJawab;
 }
