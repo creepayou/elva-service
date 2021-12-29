820cdb5421ec713e9f3f20042dde471fa6b99d2d
Eric <eric.it@rsmurniteguh.com>
2021-12-29 10:11:47 +0700

    BpjsSepDto: Penyesuaian model sep terbaru

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/dto/model/BpjsSepDto.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/dto/model/BpjsSepDto.java
@@ -11,6 +11,7 @@ import com.rsmurniteguh.bpjs.bpjsservice.config.CustomJsonDateDeserializer;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.Indikator;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.JenisPelayanan;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.KelasRawat;
+import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsEnum.Lakalantas;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsPesertaResponseDto.Informasi;
 
 import lombok.Data;
@@ -39,4 +40,36 @@ public class BpjsSepDto {
     @JsonAlias("tglPlgSEP")
     @JsonDeserialize(using = CustomJsonDateDeserializer.class)
     private Timestamp tglPlgSep;
+    private String noRujukan;
+    private Lakalantas kdStatusKecelakaan;
+    private String nmstatusKecelakaan;
+    private LokasiKejadian lokasiKejadian;
+    private DokterDPJP dpjp;
+    private BpjsKelasRawatDto klsRawat;
+    private DokterDPJP kontrol;
+    private Indikator cob;
+    private Indikator katarak;
+
+    @Data
+    @Accessors(chain = true)
+    public static class LokasiKejadian{
+        private String kdProp;
+        private String kdKab;
+        private String kdKec;
+        private String ketKejadian;
+        private String lokasi;
+        @JsonDeserialize(using = CustomJsonDateDeserializer.class)
+        private Timestamp tglKejadian;
+    }
+
+    @Data
+    @Accessors(chain = true)
+    public static class DokterDPJP{
+        @JsonAlias("kdDPJP")
+        private String kdDokter;
+        @JsonAlias("nmDPJP")
+        private String nmDokter;
+        private String noSurat;
+    }
+
 }
