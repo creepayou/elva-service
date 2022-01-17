43444ffccd6b247410a368024ea061e13fbda210
pipin <pipin@rsmurniteguh.com>
2022-01-17 13:30:42 +0700

    merapikan param - aplicares
    (cherry picked from commit 473da5ab2127642e286c15da538a53b73205602a)

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/proxy/AplicaresProxy.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/proxy/AplicaresProxy.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,25 +22,27 @@ import com.rsmurniteguh.bpjs.bpjsservice.dto.request.RequestSpriDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.response.AplicaresResponse;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.response.AplicaresResponse2;
 
+
 @FeignClient(name = "aplicares", url = "${proxy.aplicaresrest.host}", configuration = BpjsRequestConfig.class)
 public interface AplicaresProxy {
 
-	@GetMapping("/bed/read/0038R091/1/1000")
-	public AplicaresResponse<List<BpjsListKamarDto>> getListKamar(@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode);
+	
+	@GetMapping("/bed/read/{param}/1/1000")
+	public AplicaresResponse<List<BpjsListKamarDto>> getListKamar(@PathVariable("param") String parameter,@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode);
 
 	@GetMapping("/ref/kelas")
 	public AplicaresResponse<List<BpjsKodeKamarDto>> getKodeKamar(@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode);
 	
-	@PostMapping("/bed/create/0038R091")
-	public AplicaresResponse2<Object> createRoom(@RequestBody RequestAplicaresDto requestAplicaresDto,
+	@PostMapping("/bed/create/{param}")
+	public AplicaresResponse2<Object> createRoom(@PathVariable("param") String parameter,@RequestBody RequestAplicaresDto requestAplicaresDto,
 			@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode);
 	
-	@PostMapping("/bed/update/0038R091")
-	public AplicaresResponse2<Object> updateRoom(@RequestBody RequestAplicaresDto requestAplicaresDto,
+	@PostMapping("/bed/update/{param}")
+	public AplicaresResponse2<Object> updateRoom(@PathVariable("param") String parameter,@RequestBody RequestAplicaresDto requestAplicaresDto,
 			@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode);
 	
-	@PostMapping("/bed/delete/0038R091")
-	public AplicaresResponse2<Object> deleteRoom(@RequestBody RequestAplicaresDeleteDto requestAplicaresDeleteDto,
+	@PostMapping("/bed/delete/{param}")
+	public AplicaresResponse2<Object> deleteRoom(@PathVariable("param") String parameter,@RequestBody RequestAplicaresDeleteDto requestAplicaresDeleteDto,
 			@RequestHeader(Constant.MT_ENTITY_CODE) String entityCode);
 	
 }
