bcd7ffdd9a143d7b5dc484901539d51a7883b80e
Eric <eric.it@rsmurniteguh.com>
2022-01-18 11:48:46 +0700

    Aplicares: Merapikan code

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/dto/request/RequestAplicaresDeleteDto.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/dto/request/RequestAplicaresDeleteDto.java
@@ -1,12 +1,7 @@
 package com.rsmurniteguh.bpjs.bpjsservice.dto.request;
 
-import java.sql.Timestamp;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.rsmurniteguh.bpjs.bpjsservice.config.CustomJsonDateSerializer;
-import com.rsmurniteguh.bpjs.bpjsservice.config.CustomNullSerializer;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
