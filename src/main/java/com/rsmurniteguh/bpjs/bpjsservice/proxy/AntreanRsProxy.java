109d62821c7badbca4fb1ad75c43d61cd7a46541
Eric <eric.it@rsmurniteguh.com>
2022-03-09 14:44:39 +0700

    AntranRsProxy: Penambahan untuk get ref poli

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/proxy/AntreanRsProxy.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/proxy/AntreanRsProxy.java
@@ -6,8 +6,13 @@ import com.rsmurniteguh.bpjs.bpjsservice.config.BpjsRequestConfig;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.response.BpjsResponse;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @FeignClient(name = Constant.ANTREAN_FEIGN_NAME, url = "${proxy.antreanrs.host}", configuration = {BpjsRequestConfig.class, BpjsRequestClientConfig.class})
 public interface AntreanRsProxy {
+    
+    @GetMapping(value="/ref/poli")
+    public BpjsResponse<Object> getReferensiPoli();
     
 }
