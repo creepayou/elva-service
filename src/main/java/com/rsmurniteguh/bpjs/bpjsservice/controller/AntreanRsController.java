9d092aad2fbbd4894ed5b5c45742058c76b0ed10
Eric <eric.it@rsmurniteguh.com>
2022-03-14 08:36:43 +0700

    Remove unused import

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/controller/AntreanRsController.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/controller/AntreanRsController.java
@@ -8,18 +8,14 @@ import com.rsmurniteguh.bpjs.bpjsservice.base.model.ResponseSts;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsAntreanDoctorDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsAntreanPoliDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsAntreanScheduleDoctorDto;
-import com.rsmurniteguh.bpjs.bpjsservice.dto.model.SpriDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.RequestJadwalDokterDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.RequestJadwalDokterModelDto;
-import com.rsmurniteguh.bpjs.bpjsservice.dto.request.RequestSpriDto;
 import com.rsmurniteguh.bpjs.bpjsservice.exception.BusinessException;
 import com.rsmurniteguh.bpjs.bpjsservice.proxy.AntreanRsProxy;
 import com.rsmurniteguh.bpjs.bpjsservice.util.BpjsResponseUtil;
-import com.rsmurniteguh.bpjs.bpjsservice.util.VClaimResponseUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
