ff11a729d64e80f702e7088132ac6fd72d3f5826
Eric <eric.it@rsmurniteguh.com>
2022-03-15 14:02:22 +0700

    AntreanRs: Penambahan model agar batal antrean tidak perlu kirim terlalu banyak model sewaktu get

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/controller/AntreanRsController.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/controller/AntreanRsController.java
@@ -13,6 +13,7 @@ import com.rsmurniteguh.bpjs.bpjsservice.dto.model.BpjsAntreanScheduleDoctorDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.BpjsAntreanDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.BpjsAntreanModelDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.BpjsBatalAntreanDto;
+import com.rsmurniteguh.bpjs.bpjsservice.dto.request.BpjsBatalAntreanModelDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.RequestAntreanDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.RequestAntreanModelDto;
 import com.rsmurniteguh.bpjs.bpjsservice.dto.request.RequestJadwalDokterDto;
@@ -54,114 +55,120 @@ public class AntreanRsController extends BaseController {
         return ResponseSts
                 .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.getReferensiDokter(entityCode)));
     }
-    
+
     @GetMapping("/getReferensiJadwalDokter")
     public ResponseSts<List<BpjsAntreanScheduleDoctorDto>> getReferensiJadwalDokter(
-    		@RequestParam(value = "kodePoli", required = true) String kodePoli,
-    		@RequestParam(value = "tanggal", required = true) String tanggal,
+            @RequestParam(value = "kodePoli", required = true) String kodePoli,
+            @RequestParam(value = "tanggal", required = true) String tanggal,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode)
             throws BusinessException {
         return ResponseSts
-                .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.getReferensiJadwalDokter(kodePoli, tanggal, entityCode)));
+                .onSuccess(BpjsResponseUtil
+                        .handleBpjsResponse(antreanRsProxy.getReferensiJadwalDokter(kodePoli, tanggal, entityCode)));
     }
-    
+
     @PostMapping("/updateJadwalDokter")
     public ResponseSts<Object> updateJadwalDokter(@RequestBody RequestJadwalDokterModelDto requestJadwalDokterModelDto,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) throws BusinessException {
-    	RequestJadwalDokterDto requestJadwalDokterDto = new RequestJadwalDokterDto();
-    	requestJadwalDokterDto.setKodepoli(requestJadwalDokterModelDto.getKodePoli());
-    	requestJadwalDokterDto.setKodedokter(requestJadwalDokterModelDto.getKodeDokter());
-    	requestJadwalDokterDto.setKodesubspesialis(requestJadwalDokterModelDto.getKodeSubSpesialis());
-    	requestJadwalDokterDto.setJadwal(requestJadwalDokterModelDto.getJadwal());
-    	return ResponseSts
-                .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.updateJadwalDokter(requestJadwalDokterDto, entityCode)));
+        RequestJadwalDokterDto requestJadwalDokterDto = new RequestJadwalDokterDto();
+        requestJadwalDokterDto.setKodepoli(requestJadwalDokterModelDto.getKodePoli());
+        requestJadwalDokterDto.setKodedokter(requestJadwalDokterModelDto.getKodeDokter());
+        requestJadwalDokterDto.setKodesubspesialis(requestJadwalDokterModelDto.getKodeSubSpesialis());
+        requestJadwalDokterDto.setJadwal(requestJadwalDokterModelDto.getJadwal());
+        return ResponseSts
+                .onSuccess(BpjsResponseUtil
+                        .handleBpjsResponse(antreanRsProxy.updateJadwalDokter(requestJadwalDokterDto, entityCode)));
 
     }
-    
+
     @PostMapping("/getListTask")
     public ResponseSts<List<BpjsAntreanListTaskDto>> getListTask(@RequestBody RequestListTaskDto requestListTaskDto,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) throws BusinessException {
-    	RequestListTaskModelDto requestListTaskModelDto = new RequestListTaskModelDto();
-    	requestListTaskModelDto.setKodebooking(requestListTaskDto.getKodeBooking());
-    	return ResponseSts
-                .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.getListTask(requestListTaskModelDto, entityCode)));
+        RequestListTaskModelDto requestListTaskModelDto = new RequestListTaskModelDto();
+        requestListTaskModelDto.setKodebooking(requestListTaskDto.getKodeBooking());
+        return ResponseSts
+                .onSuccess(BpjsResponseUtil
+                        .handleBpjsResponse(antreanRsProxy.getListTask(requestListTaskModelDto, entityCode)));
 
     }
-    
+
     @GetMapping("/getDashboardPerTanggal")
     public ResponseSts<List<BpjsAntreanDashboardPerTanggalDto>> getDashboardPerTanggal(
-    		@RequestParam(value = "tanggal", required = true) String tanggal,
-    		@RequestParam(value = "waktu", required = true) String waktu,
+            @RequestParam(value = "tanggal", required = true) String tanggal,
+            @RequestParam(value = "waktu", required = true) String waktu,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode)
             throws BusinessException {
         return ResponseSts
-                .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.getDashboardPerTanggal(tanggal, waktu, entityCode)));
+                .onSuccess(BpjsResponseUtil
+                        .handleBpjsResponse(antreanRsProxy.getDashboardPerTanggal(tanggal, waktu, entityCode)));
     }
-    
+
     @GetMapping("/getDashboardPerBulan")
     public ResponseSts<List<BpjsAntreanDashboardPerTanggalDto>> getDashboardPerBulan(
-    		@RequestParam(value = "bulan", required = true) String bulan,
-    		@RequestParam(value = "tahun", required = true) String tahun,
-    		@RequestParam(value = "waktu", required = true) String waktu,
+            @RequestParam(value = "bulan", required = true) String bulan,
+            @RequestParam(value = "tahun", required = true) String tahun,
+            @RequestParam(value = "waktu", required = true) String waktu,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode)
             throws BusinessException {
         return ResponseSts
-                .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.getDashboardPerBulan(bulan, tahun, waktu, entityCode)));
+                .onSuccess(BpjsResponseUtil
+                        .handleBpjsResponse(antreanRsProxy.getDashboardPerBulan(bulan, tahun, waktu, entityCode)));
     }
-    
+
     @PostMapping("/updateWaktuAntrean")
     public ResponseSts<Object> updateWaktuAntrean(@RequestBody RequestAntreanModelDto requestAntreanModelDto,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) throws BusinessException {
-    	RequestAntreanDto requestAntreanDto = new RequestAntreanDto();
-    	requestAntreanDto.setKodebooking(requestAntreanModelDto.getKodeBooking());
-    	requestAntreanDto.setTaskid(requestAntreanModelDto.getTaskId());
-    	requestAntreanDto.setWaktu(requestAntreanModelDto.getWaktu());
-    	return ResponseSts
-                .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.updateWaktuAntrean(requestAntreanDto, entityCode)));
+        RequestAntreanDto requestAntreanDto = new RequestAntreanDto();
+        requestAntreanDto.setKodebooking(requestAntreanModelDto.getKodeBooking());
+        requestAntreanDto.setTaskid(requestAntreanModelDto.getTaskId());
+        requestAntreanDto.setWaktu(requestAntreanModelDto.getWaktu());
+        return ResponseSts
+                .onSuccess(BpjsResponseUtil
+                        .handleBpjsResponse(antreanRsProxy.updateWaktuAntrean(requestAntreanDto, entityCode)));
 
     }
-    
+
     @PostMapping("/tambahAntrean")
     public ResponseSts<Object> tambahAntrean(@RequestBody BpjsAntreanModelDto model,
             @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) throws BusinessException {
-    	BpjsAntreanDto bpjsDto = new BpjsAntreanDto();
-    	bpjsDto.setKodebooking(model.getKodeBooking());
-    	bpjsDto.setJenispasien(model.getJenisPasien());
-    	bpjsDto.setNomorkartu(model.getNomorKartu());
-    	bpjsDto.setNik(model.getNik());
-    	bpjsDto.setNohp(model.getNoHp());
-    	bpjsDto.setKodepoli(model.getKodePoli());
-    	bpjsDto.setNamapoli(model.getNamaPoli());
-    	bpjsDto.setPasienbaru(model.getPasienBaru());
-    	bpjsDto.setNorm(model.getNorm());
-    	bpjsDto.setTanggalperiksa(model.getTanggalPeriksa());
-    	bpjsDto.setKodedokter(model.getKodeDokter());
-    	bpjsDto.setNamadokter(model.getNamaDokter());
-    	bpjsDto.setJampraktek(model.getJamPraktek());
-    	bpjsDto.setJeniskunjungan(model.getJenisKunjungan());
-    	bpjsDto.setNomorreferensi(model.getNomorReferensi());
-    	bpjsDto.setNomorantrean(model.getNomorAntrean());
-    	bpjsDto.setAngkaantrean(model.getAngkaAntrean());
-    	bpjsDto.setEstimasidilayani(model.getEstimasiDilayani());
-    	bpjsDto.setSisakuotajkn(model.getSisaKuotaJkn());
-    	bpjsDto.setKuotajkn(model.getKuotaJkn());
-    	bpjsDto.setSisakuotanonjkn(model.getSisakuotaNonJkn());
-    	bpjsDto.setKuotanonjkn(model.getKuotaNonJkn());
-    	bpjsDto.setKeterangan(model.getKeterangan());
-    	return ResponseSts
+        BpjsAntreanDto bpjsDto = new BpjsAntreanDto();
+        bpjsDto.setKodebooking(model.getKodeBooking());
+        bpjsDto.setJenispasien(model.getJenisPasien());
+        bpjsDto.setNomorkartu(model.getNomorKartu());
+        bpjsDto.setNik(model.getNik());
+        bpjsDto.setNohp(model.getNoHp());
+        bpjsDto.setKodepoli(model.getKodePoli());
+        bpjsDto.setNamapoli(model.getNamaPoli());
+        bpjsDto.setPasienbaru(model.getPasienBaru());
+        bpjsDto.setNorm(model.getNorm());
+        bpjsDto.setTanggalperiksa(model.getTanggalPeriksa());
+        bpjsDto.setKodedokter(model.getKodeDokter());
+        bpjsDto.setNamadokter(model.getNamaDokter());
+        bpjsDto.setJampraktek(model.getJamPraktek());
+        bpjsDto.setJeniskunjungan(model.getJenisKunjungan());
+        bpjsDto.setNomorreferensi(model.getNomorReferensi());
+        bpjsDto.setNomorantrean(model.getNomorAntrean());
+        bpjsDto.setAngkaantrean(model.getAngkaAntrean());
+        bpjsDto.setEstimasidilayani(model.getEstimasiDilayani());
+        bpjsDto.setSisakuotajkn(model.getSisaKuotaJkn());
+        bpjsDto.setKuotajkn(model.getKuotaJkn());
+        bpjsDto.setSisakuotanonjkn(model.getSisakuotaNonJkn());
+        bpjsDto.setKuotanonjkn(model.getKuotaNonJkn());
+        bpjsDto.setKeterangan(model.getKeterangan());
+        return ResponseSts
                 .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.tambahAntrean(bpjsDto, entityCode)));
 
     }
-    
-    @PostMapping("/batalAntrean")
-    public ResponseSts<Object> batalAntrean(@RequestBody BpjsAntreanModelDto model,
-            @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) throws BusinessException {
-    	BpjsBatalAntreanDto bpjsDto = new BpjsBatalAntreanDto();
-    	bpjsDto.setKodebooking(model.getKodeBooking());
-    	bpjsDto.setKeterangan(model.getKeterangan());
-    	return ResponseSts
-                .onSuccess(BpjsResponseUtil.handleBpjsResponse(antreanRsProxy.batalAntrean(bpjsDto, entityCode)));
 
+    @PostMapping("/batalAntrean")
+    public ResponseSts<Object> batalAntrean(@RequestBody BpjsBatalAntreanModelDto bpjsBatalAntreanModelDto,
+            @RequestHeader(Constant.MT_ENTITY_CODE) String entityCode) throws BusinessException {
+        BpjsBatalAntreanDto bpjsBatalAntreanDto = new BpjsBatalAntreanDto()
+                .setKodebooking(bpjsBatalAntreanModelDto.getKodeBooking())
+                .setKeterangan(bpjsBatalAntreanModelDto.getKeterangan());
+        return ResponseSts
+                .onSuccess(BpjsResponseUtil
+                        .handleBpjsResponse(antreanRsProxy.batalAntrean(bpjsBatalAntreanDto, entityCode)));
     }
-    
+
 }
