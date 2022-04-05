85f8f9ed3879c9688e7b9a6c8a8eb7ff188fbe3a
Eric <eric.it@rsmurniteguh.com>
2022-04-05 15:11:41 +0700

    DateUtil: Penyesuaian agar convert berdasarkan offset sekarang

--- src/main/java/com/rsmurniteguh/bpjs/bpjsservice/util/DateUtil.java
+++ src/main/java/com/rsmurniteguh/bpjs/bpjsservice/util/DateUtil.java
@@ -2,60 +2,79 @@ package com.rsmurniteguh.bpjs.bpjsservice.util;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
-    private DateUtil(){
+    private DateUtil() {
         throw new IllegalStateException("Utility Class");
     }
 
     private static final String DATE_PATTERN = "yyyy-MM-dd";
     private static final String DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATETIMETZ_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
-    public static String customFormatTimestamp(Timestamp timestamp, String pattern){
+    public static String customFormatTimestamp(Timestamp timestamp, String pattern) {
         return new SimpleDateFormat(pattern).format(timestamp);
     }
 
-    public static String formatTimestamp(Timestamp timestamp){
+    public static String formatTimestamp(Timestamp timestamp) {
         return customFormatTimestamp(timestamp, DATE_PATTERN);
     }
 
-    public static String formatTimestamp2(Timestamp timestamp){
+    public static String formatTimestamp2(Timestamp timestamp) {
         return customFormatTimestamp(timestamp, DATETIME_PATTERN);
     }
 
-    public static String customFormatTimestampWithTimezone(Timestamp timestamp, String timezone, String pattern){
-        return ZonedDateTime.ofInstant(timestamp.toInstant(), ZoneId.of(timezone)).toLocalDateTime().format(DateTimeFormatter.ofPattern(pattern));
+    public static String customFormatTimestampWithTimezone(Timestamp timestamp, String timezone, String pattern) {
+        return ZonedDateTime.ofInstant(timestamp.toInstant(), ZoneId.of(timezone)).toLocalDateTime()
+                .format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    public static String formatTimestampWithTimezone(Timestamp timestamp, String timezone){
+    public static String formatTimestampWithTimezone(Timestamp timestamp, String timezone) {
         return customFormatTimestampWithTimezone(timestamp, timezone, DATE_PATTERN);
     }
 
-    public static String formatTimestampWithTimezone2(Timestamp timestamp, String timezone){
+    public static String formatTimestampWithTimezone2(Timestamp timestamp, String timezone) {
         return customFormatTimestampWithTimezone(timestamp, timezone, DATETIME_PATTERN);
     }
 
-    public static Timestamp customFormatStringWithTimezone(String string, String fromTimezone, String pattern){
+    public static String formatTimestampWithTimezone3(Timestamp timestamp, String timezone) {
+        return customFormatTimestampWithTimezone(timestamp, timezone, DATETIMETZ_PATTERN);
+    }
+
+    private static Timestamp formatLocalDateTimeWithTimezone(LocalDateTime localDateTime, String fromTimezone) {
+        ZonedDateTime zonedDateTime = localDateTime.atOffset(convertToZoneOffset(ZoneId.of(fromTimezone)))
+                .toZonedDateTime();
+
+        return Timestamp.valueOf(zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime());
+    }
+
+    public static Timestamp customFormatStringWithTimezone(String string, String fromTimezone, String pattern) {
         LocalDateTime localDateTime = LocalDateTime.parse(string, DateTimeFormatter.ofPattern(pattern));
-        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.of(fromTimezone));
-
-        return Timestamp.valueOf(zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime()); 
+        return formatLocalDateTimeWithTimezone(localDateTime, fromTimezone);
     }
 
-    public static Timestamp formatStringWithTimezone(String string, String fromTimezone){
+    private static ZoneOffset convertToZoneOffset(ZoneId zoneId) {
+        return zoneId.getRules().getOffset(Instant.now());
+    }
+
+    public static Timestamp formatStringWithTimezone(String string, String fromTimezone) {
         LocalDate localDate = LocalDate.parse(string, DateTimeFormatter.ofPattern(DATE_PATTERN));
-        ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZoneId.of(fromTimezone));
-
-        return Timestamp.valueOf(zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime()); 
+        return formatLocalDateTimeWithTimezone(localDate.atStartOfDay(), fromTimezone);
     }
 
-    public static Timestamp formatStringWithTimezone2(String string, String fromTimezone){
+    public static Timestamp formatStringWithTimezone2(String string, String fromTimezone) {
         return customFormatStringWithTimezone(string, fromTimezone, DATETIME_PATTERN);
+    }
+
+    public static Timestamp formatStringWithTimezone3(String string, String fromTimezone) {
+        return customFormatStringWithTimezone(string, fromTimezone, DATETIMETZ_PATTERN);
     }
 }
